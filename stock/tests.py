4a85659af47167d710a697523fa910a989bb6dae
Nam-gu <skarn2158@naver.com>
2023-03-05 18:10:01 +0900

    test: Initial test

--- stock/tests.py
+++ stock/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from rest_framework.test import APIClient
+from rest_framework import status
+
+class ViewTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.response = self.client.get('/stock/hello/')
+
+    def test_api_get_hello(self):
+        self.assertEqual(self.response.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.response.data, "hello world!")
