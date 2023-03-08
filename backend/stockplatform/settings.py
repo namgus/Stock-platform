b9a17b83ad26ad4477cc22651f30ab2edf59190f
Nam-gu <skarn2158@naver.com>
2023-03-08 20:30:05 +0900

    fix: Change cors whitelist to list

--- backend/stockplatform/settings.py
+++ backend/stockplatform/settings.py
@@ -121,5 +121,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-CORS_ORIGIN_WHITELIST = ('http://127.0.0.1:3000', 'http://localhost:3000')
+CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000', 'http://localhost:3000']
 CORS_ALLOW_CREDENTIALS = True
