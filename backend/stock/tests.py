46ab57fce71ee944f593d0f3d7ff4844f9d7b1d8
Nam-gu <skarn2158@naver.com>
2023-03-27 01:19:21 +0900

    test: Add price api test code

--- backend/stock/tests.py
+++ backend/stock/tests.py
@@ -3,11 +3,32 @@ from django.test import TestCase
 from rest_framework.test import APIClient
 from rest_framework import status
 
-class ViewTest(TestCase):
+class HelloTest(TestCase):
     def setUp(self):
         self.client = APIClient()
-        self.response = self.client.get('/stock/hello/')
 
     def test_api_get_hello(self):
-        self.assertEqual(self.response.status_code, status.HTTP_200_OK)
-        self.assertEqual(self.response.data, "hello world!")
+        response = self.client.get('/stock/hello/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, "hello world!")
+
+class PriceTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_price_api_with_default_code(self):
+        response = self.client.get('/stock/price/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('price', response.data)
+
+    def test_price_api_with_custom_code(self):
+        code = "005930"
+        response = self.client.get(f'/stock/price/?code={code}')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertGreater(self.data['price'], 0)
+        self.assertIn('price', response.data)
+
+    def test_price_api_with_invalid_code(self):
+        code = "000000"
+        response = self.client.get(f'/stock/price/?code={code}')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
