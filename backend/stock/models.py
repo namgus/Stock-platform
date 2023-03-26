3bc2177ac148264cdb0a5b066e6639b7527fc061
Nam-gu <skarn2158@naver.com>
2023-03-27 03:00:23 +0900

    test: Add comment api test code

--- backend/stock/models.py
+++ backend/stock/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Comment(models.Model):
     id = models.AutoField(primary_key=True, null=False, blank=False)
-    code = models.IntegerField()
+    code = models.CharField(max_length=6)
     created_at = models.DateTimeField(auto_now_add=True)
     comments = models.TextField()
--- backend/stock/tests.py
+++ backend/stock/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from rest_framework.test import APIClient
 from rest_framework import status
 
+from .models import Comment
 
 class HelloTest(TestCase):
     def setUp(self):
@@ -104,3 +105,34 @@ class FinancialTest(TestCase):
         code = "000000"
         response = self.client.get(f'/stock/financial/?code={code}')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+class CommentTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.comment1 = Comment.objects.create(code='005930', comments='Test comment 1')
+        self.comment2 = Comment.objects.create(code='005930', comments='Test comment 2')
+        self.comment3 = Comment.objects.create(code='041510', comments='Test comment 3')
+
+    def test_get_comments(self):
+        response = self.client.get('/stock/comment/?code=005930')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+    def test_create_comment(self):
+        comment_data = {'code': '005930', 'comments': 'Test comment 4'}
+        response = self.client.post('/stock/comment/', data=comment_data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Comment.objects.count(), 4)
+
+        comment = Comment.objects.get(id=response.data['id'])
+        self.assertEqual(comment.code, comment_data['code'])
+        self.assertEqual(comment.comments, comment_data['comments'])
+
+    def test_create_invalid_without_code(self):
+        comment_data = {'comments': 'Test comment 5'}
+        response = self.client.post('/stock/comment/', data=comment_data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(Comment.objects.count(), 3)
