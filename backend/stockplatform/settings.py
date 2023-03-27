1b246ce2c2075a04f7c4dee76bb7b04eaef1a68f
Nam-gu <skarn2158@naver.com>
2023-03-28 02:13:22 +0900

    fix: Set for test db correctly

--- backend/stockplatform/settings.py
+++ backend/stockplatform/settings.py
@@ -90,14 +90,19 @@ DATABASES = {
         "USER": "admin",
         "PASSWORD": "skarn1234!",
         "HOST": "stock-database.chqkhm2nn9io.ap-northeast-2.rds.amazonaws.com",
-        "PORT": "3306"
-    },
-    "test": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "PORT": "3306",
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
+import sys
+if 'test' in sys.argv:
+    DATABASES['default'] = {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "testdbsqlite3",
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
