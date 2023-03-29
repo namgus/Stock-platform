c12110089b597523c3393ca60c3a768ebbe1b129
Nam-gu <skarn2158@naver.com>
2023-03-29 23:24:17 +0900

    chore: Remove comment

--- backend/stockplatform/settings.py
+++ backend/stockplatform/settings.py
@@ -77,12 +77,6 @@ WSGI_APPLICATION = "stockplatform.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
