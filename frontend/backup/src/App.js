5ca873348ca82dd68d2b84e6c8501b28bc367f88
Nam-gu <skarn2158@naver.com>
2023-04-03 17:26:47 +0900

    chore: Change to typescript ver.

---
+++ frontend/backup/src/App.js
@@ -0,0 +1,63 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React, { useState } from "react";
+import axios from "axios";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <RestAPI></RestAPI>
+      </header>
+    </div>
+  );
+}
+
+function RestAPI() {
+  const [text, setText] = useState("");
+
+  return (
+    <>
+      <div>
+        <button onClick={()=>{
+          axios.get("http://15.164.227.80:8000/stock/hello")
+          .then(function (response) {
+            console.log(response);
+            { setText("test") };
+          })
+        }}
+        >button</button>
+      </div>
+
+      <div>
+        <button onClick={()=>{
+          axios.get("http://127.0.0.1:8000/stock/hello")
+          .then(function (response) {
+            console.log(response);
+          })
+        }}
+        >button</button>
+      </div>
+
+      <div> 
+        {text}
+      </div>
+    </>
+  )
+}
+
+
+export default App;
