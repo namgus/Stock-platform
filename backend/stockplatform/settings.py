56af2345a8247a14f65a2d26ad7b6a3b7e6f640d
Nam-gu <skarn2158@naver.com>
2023-04-21 03:09:23 +0900

    fix: Change origin whitelist

--- backend/stockplatform/settings.py
+++ backend/stockplatform/settings.py
@@ -137,9 +137,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'http://43.201.185.173:3000',
-    'http://stockku.shop:3000',
-    'http://stockku.shop:80'
+    'http://43.201.185.173',
+    'http://stockku.shop',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
